ee8772eb095ce9c04135517eb397f5d57ed7f9a5
Konst-b <konst_b@bk.ru>
2018-10-04 15:27:17 +0300

    dz

---
+++ src/main/java/first_hometask/Recursion.java
@@ -0,0 +1,11 @@
+package first_hometask;
+
+public class Recursion {
+    static void showNumChain(int n){
+        if (n>1) showNumChain(n-1);
+        System.out.print(n+" ");
+    }
+    public static void main(String[] args){
+        showNumChain(1234);
+    }
+}
